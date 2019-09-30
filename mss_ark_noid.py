b8ca1c3d293f0508d08474810b892586727359ef
Ashton Prigge <ashton.prigge@gmail.com>
2019-09-30 14:06:47 -0700

    changed file name

--- mss_ark_noid.py
+++ mss_ark_noid.py
@@ -3,15 +3,8 @@ import os
 import subprocess
 import pandas as pd
 
-#Read the given csv file into the pandas DataFrame
-# the four inputs the function takes are the file path to the csv input file
-#the configured yml file which sets up the specifics for the ark, the parent ark, and
-#the name of the output file
 
-#need to add subprocess-- if row type is equal to work, run ezid script
-#at end of that, call the create_arks
-
-
+#creates a mappings file that provides metadata to EZID
 def create_mappings():
 
 	cursor = csv.DictReader(open(file_path),
@@ -25,16 +18,15 @@ def create_mappings():
 			for s in string:
 				mappings_file.write(s)
 
+#creates a noid format file that suplies the parent ark for the NOID to be appended to
 def create_noid_yml(parent_ark):
-		#parent_ark = read csv and manuscript row
+
 	noid_file = open("Noid_test.yml", "w+")
 	string = ['template: eeddeede \n',('scheme: ' + str(parent_ark[0:11])), ('\nnaa: ' + str(parent_ark[11:]))]
 	for s in string:
 		noid_file.write(s)
 
-
-	#Writes DataFrame to a new csv file
-
+#calls the EZID and NOID scripts based if the row is a Work or Page
 def call_ezid(file_path):
 
 	cursor = csv.DictReader(open(file_path),
@@ -42,6 +34,7 @@ def call_ezid(file_path):
 
 	item_ark_list = []
 	parent_ark_list = ['']
+
 	for row in cursor:
 		if row['Object type'] == 'Work':
 			create_mappings()
@@ -53,29 +46,29 @@ def call_ezid(file_path):
 			item_ark_list.append(parent_ark)
 			
 
-
 		if row['Object type'] == 'Page':
-			#create_arks(file_path, 'Noid_test.yml', parent_ark, output_file)
 			cmd = ['noid', '-f', 'Noid_test.yml']
 			item_ark = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0]
 			print(item_ark)
 			item_ark_list.append(item_ark)
 			parent_ark_list.append(parent_ark)
-	print(item_ark_list)
+
+#writes the new parent ARKs and Item ARKs to new columns in an export csv
 	data= pd.read_csv(file_path, sep=',', delimiter=None, header='infer')
 	data['Parent Ark'] = parent_ark_list
 	data['Item Ark'] = item_ark_list
 	data.to_csv(path_or_buf=output_file, sep=',', na_rep='', float_format=None, index=False)
 
 
-
-
+#input from the command line to supply directory of files,
+# the EZID credientials, and the preferred ARK shoulder
 
 directory = raw_input('File directory:')
 ezid_input = raw_input('EZID username and password:')
 ark_shoulder = raw_input('ARK shoulder:')
 				
-			
+
+#runs through the functions for all csvs in the directory supplied			
 for filename in os.listdir(directory):
 	if '.csv' in filename:
 		file_path = directory + (str(filename))
